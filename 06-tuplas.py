abfd44fc7a8f0597c84a555cca3539f5543103db
Leomh71832 <71832leo@universidaddeleon.edu.mx>
2026-02-12 09:07:54 -0600

    segundoparcial

---
+++ 06-tuplas.py
@@ -0,0 +1,15 @@
+
+tupla=(1,2,3,4,5)
+
+print(type(tupla))
+print(tupla)
+print("El Elemento de la tupla es: ",tupla[2])
+
+for i in tupla:
+    print(i)
+    
+print("La Cantidad de elementos en la tupla es: ",len(tupla))
+print("La Cantidad de veces que se repite el num 2 es: ",len.count(2))
+print("El indice del num 3 es: ", tupla.index(3))
+
+tupla[1]=2
